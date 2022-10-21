6b334c5133ef4f1947e4ce848a1651d898821044
(SSB-61B9) Quyen Tran <quyen.tran@ssbshoes.com>
2022-10-21 10:39:13 +0700

    add main page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,71 @@
-let menu = document.querySelector('#menu-bars');
-let mavbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bars");
+let mavbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    mavbar.classList.toggle('active');
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  mavbar.classList.toggle("active");
+};
 
+let section = document.querySelector("section");
+let navLinks = document.querySelector("header .navbar a");
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  mavbar.classList.remove("active");
+};
+
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  canteredSlides: true,
+  autoplay: {
+    deplay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: "swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 30,
+  canteredSlides: true,
+  autoplay: {
+    deplay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out')
+}
+function fadeOut(){
+    setInterval(loader,3000)
 }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    mavbar.classList.remove('active');
-    
-}
-
-document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active')
-}
-
-document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.remove('active')
-}
+window.onload =fadeOut;
